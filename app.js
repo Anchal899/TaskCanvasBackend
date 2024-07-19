eb47d28b36553056a33f85471d1f97890c3c7c99
Anchal899 <101783656+Anchal899@users.noreply.github.com>
2024-07-19 19:09:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,13 +9,7 @@ require('dotenv').config();
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:4200', 'https://task-canvas-frontend1.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+
 app.use(routes);
 
 const corsOptions = {
